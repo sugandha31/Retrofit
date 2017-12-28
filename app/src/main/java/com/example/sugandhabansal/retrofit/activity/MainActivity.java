08f9a36c2e66174150b9fb8bfb331e33fbd33efb
Sugandha Bansal <bansal.sugandha@gmail.com>
2017-12-28 22:37:22 +0530

    complete

--- app/src/main/java/com/example/sugandhabansal/retrofit/activity/MainActivity.java
+++ app/src/main/java/com/example/sugandhabansal/retrofit/activity/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.sugandhabansal.retrofit.activity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.example.sugandhabansal.retrofit.R;
+import com.example.sugandhabansal.retrofit.adapter.MoviesAdapter;
 import com.example.sugandhabansal.retrofit.model.Movies;
 import com.example.sugandhabansal.retrofit.model.MoviesResponse;
 import com.example.sugandhabansal.retrofit.rest.ApiClient;
@@ -33,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        @SuppressLint("WrongViewCast") final RecyclerView recyclerView = findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         ApiInterface apiInterface = ApiClient.getClient().create(ApiInterface.class);
         Call<MoviesResponse> call = apiInterface.getTopRatedMovies(API_KEY);
 
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
                 List<Movies> movies = response.body().getResults();
                 Log.d(TAG,"Number of movies recieved: "+movies.size());
+                recyclerView.setAdapter(new MoviesAdapter(movies,R.layout.list_item_movie,getApplicationContext()));
             }
 
             @Override
--- app/src/main/java/com/example/sugandhabansal/retrofit/adapter/MoviesAdapter.java
+++ app/src/main/java/com/example/sugandhabansal/retrofit/adapter/MoviesAdapter.java
@@ -1,8 +1,61 @@
 package com.example.sugandhabansal.retrofit.adapter;
 
-/**
- * Created by Sugandha Bansal on 12/28/2017.
- */
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MoviesAdapter {
+import com.example.sugandhabansal.retrofit.R;
+import com.example.sugandhabansal.retrofit.model.Movies;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    private List<Movies> movies;
+    private int rowLayout;
+    private Context context;
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(rowLayout,parent,false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        holder.movieTitle.setText(movies.get(position).getTitle());
+        holder.data.setText(movies.get(position).getReleaseDate());
+        holder.movieDescription.setText(movies.get(position).getOverview());
+        holder.rating.setText(movies.get(position).getVoteAverage().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        LinearLayout moviesLayout;
+        TextView movieTitle, data, movieDescription, rating;
+
+        public MovieViewHolder(View v) {
+            super(v);
+            moviesLayout = (LinearLayout) v.findViewById(R.id.movies_layout);
+            movieTitle = (TextView) v.findViewById(R.id.title);
+            data = (TextView) v.findViewById(R.id.subtitle);
+            movieDescription = (TextView) v.findViewById(R.id.description);
+            rating = (TextView) v.findViewById(R.id.rating);
+        }
+    }
+
+    public MoviesAdapter(List<Movies> movies,int rowLayout,Context context){
+        this.movies = movies;
+        this.rowLayout = rowLayout;
+        this.context = context;
+    }
 }
